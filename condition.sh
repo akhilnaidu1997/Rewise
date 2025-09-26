e057e92b1ca0fc6c4599414c30a3f5aa0bd619a6
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-27 01:09:24 +0530

    message

--- condition.sh
+++ condition.sh
@@ -4,7 +4,7 @@ NUM=$1
 
 if [ $NUM -lt 10 ]; then
     echo "Given number $NUM is less than 10"
-elif [ $NUM -eq 0 ]; then
+elif [ $NUM -eq 10 ]; then
     echo "Given number $NUM is equal to 10"
 else
     echo "Given number $NUM is greater than 10"
