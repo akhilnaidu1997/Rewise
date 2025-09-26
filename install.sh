4f7d2c13cca20a44b9398384097b10a8569b94eb
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-26 10:50:18 +0530

    message

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ USER=$(id -u)
 
 if [ $USER -ne 0 ]; then
     echo " ERROR:: You donot have sudo permissions, please proceed with sudo access"
+    exit 1
 fi
 
 dnf install nginx -y
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
     echo " ERROR: Installation is failed"
 else
     echo "Installation is successful"
