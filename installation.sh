c61ee9a444c1df03b591babeeb7f8b3fce4c662b
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-27 01:32:19 +0530

    message

--- installation.sh
+++ installation.sh
@@ -7,6 +7,12 @@ N="\e[0m"
 
 USER=$(id -u)
 
+LOG_FOLDER="/var/log/shell-practice"
+SCRIPT_NAME="echo $0 | cut -d "." -f1"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOG_FOLDER
+
 if [ $USER -ne 0 ]; then
     echo -e "Please proceed with $R sudo permissions $N"
     exit 1
@@ -21,25 +27,25 @@ fi
 }
 
 
-dnf list installed mysql
+dnf list installed mysql &>>LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install mysql -y
+    dnf install mysql -y &>>LOG_FILE
     VALIDATE $? "MYSQL"
 else
     echo -e "MYSQL already $Y installed $N"
 fi
 
-dnf list installed nginx
+dnf list installed nginx &>>LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install nginx -y
+    dnf install nginx -y &>>LOG_FILE
     VALIDATE $? "NGINX"
 else
     echo -e "NGINX already $Y installed $N"
 fi
 
-dnf list installed python3
+dnf list installed python3 &>>LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install python3 -y
+    dnf install python3 -y &>>LOG_FILE
     VALIDATE $? "PYTHON"
 else
     echo -e "PYTHON already $Y installed $N"
