930d365446d1c3ad483ffc1c0ab119bc31d5e08c
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-27 01:15:02 +0530

    message

---
+++ installation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ]; then
+    echo "ERROR:: Please proceed with sudo permissions"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installation of mysql failed"
+else
+    echo " MYSQL is already installed"
+fi
