1e347d76d0bfedf16327e4a351bd37b2a78592f3
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-27 01:35:14 +0530

    message

--- installation.sh
+++ installation.sh
@@ -9,7 +9,7 @@ USER=$(id -u)
 
 LOG_FOLDER="/var/log/shell-practice"
 SCRIPT_NAME="echo $0 | cut -d "." -f1"
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE=$($LOG_FOLDER/$SCRIPT_NAME.log)
 
 mkdir -p $LOG_FOLDER
 
