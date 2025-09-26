63513b1d281ed1bc54db37d97fb31f588c842889
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-26 23:51:39 +0530

    message

--- conditions.sh
+++ conditions.sh
@@ -3,11 +3,12 @@
 USER=$(id -u)
 if [ $USER -ne 0 ]; then
     echo "ERROR: you donot have sudo permissions to install"
+    exit 1
 fi
 
 dnf install mysql -y
 if [ $? -ne 0 ]; then
-    echo " ERROR:: Installation of mysql is failed"
+    echo "ERROR:: Installation of mysql is failed"
 else
     echo "Installation of mysql is successful"
 fi
