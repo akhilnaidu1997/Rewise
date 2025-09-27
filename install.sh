789fe343b34537473721dfdccabc95d50c2b97d6
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-27 07:48:27 +0530

    message

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 USER=$(id -u)
 if [ $USER -ne 0 ]; then
     echo "$R Please proceed with sudo permissions $N"
@@ -14,3 +15,18 @@ else
     echo " Installation of mysql is $G SUCCESSFUL $N"
 fi
 
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo " Installation of nginx is $R FAILED $N"
+else
+    echo " Installation of nginx is $G SUCCESSFUL $N"
+fi
+
+dnf install python3 -y
+
+if [ $? -ne 0 ]; then
+    echo " Installation of python is $R FAILED $N"
+else
+    echo " Installation of python is $G SUCCESSFUL $N"
+fi
