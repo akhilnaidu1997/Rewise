f7de456a0925f341e7bec4fb76eeac97acb12295
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-27 11:10:05 +0530

    message

--- instance.sh
+++ instance.sh
@@ -2,13 +2,13 @@
 
 for instance in $@
 do
-    INSTANCE_ID=$( aws ec2 run-instances --image-id ami-09c813fb71547fc4f --count 1 --instance-type t3.micro --security-group-ids sg-0fee42dfd5533e5de --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=TEST}]" )
+    INSTANCE_ID=$( aws ec2 run-instances --image-id ami-09c813fb71547fc4f --count 1 --instance-type t3.micro --security-group-ids sg-0fee42dfd5533e5de --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=TEST}]" --query 'Instances[0].InstanceId' --output text )
 done
 
-if [ instance != "frontend" ]; then
-    IP=$(aws ec2 describe-instances --instance-ids i-0eca56180912f7295 --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+if [ $instance != "frontend" ]; then
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
 else
-    IP=$(aws ec2 describe-instances --instance-ids i-0eca56180912f7295 --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
 fi
 
 aws route53 change-resource-record-sets \
