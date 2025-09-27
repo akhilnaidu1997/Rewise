e720bc0a61351fa6c175ef85abe99c93e03fb0b7
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-27 08:13:36 +0530

    message

--- install.sh
+++ install.sh
@@ -7,6 +7,14 @@ if [ $USER -ne 0 ]; then
     exit 1
 fi
 
+LOG_FOLDER="/var/log/shellshell"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOG_FOLDER 
+
+echo "Script started at : $(date)"
+
 VALIDATE(){
     if [ $1 -ne 0 ]; then
         echo " Installation of $2 is $R FAILED $N"
@@ -16,25 +24,25 @@ VALIDATE(){
 
 }
 
-dnf list installed mysql
+dnf list installed mysql $>> $LOG_FILE 
 if [ $? -ne 0 ]; then
-    dnf install mysql -y
+    dnf install mysql -y $>> $LOG_FILE
     VALIDATE $? "mysql"
 else
     echo "mysql is already installed"
 fi
 
-dnf list installed nginx
+dnf list installed nginx $>> $LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install nginx -y
+    dnf install nginx -y $>> $LOG_FILE
     VALIDATE $? "nginx"
 else
     echo "nginx is already installed"
 fi
 
-dnf list installed python3
+dnf list installed python3 $>> $LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install python3 -y
+    dnf install python3 -y $>> $LOG_FILE
     VALIDATE $? "python"
 else
     echo "python is already installed"
