ca453087fd7624e8d1ee4317f7daeb8e735beb53
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-27 11:06:07 +0530

    message

--- instance.sh
+++ instance.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-INSTANCE_ID=$( aws ec2 run-instances --image-id ami-09c813fb71547fc4f --count 1 --instance-type t3.micro --security-group-ids sg-0fee42dfd5533e5de --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=TEST}]" )
+for instance in $@
+do
+    INSTANCE_ID=$( aws ec2 run-instances --image-id ami-09c813fb71547fc4f --count 1 --instance-type t3.micro --security-group-ids sg-0fee42dfd5533e5de --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=TEST}]" )
+done
 
-if [ $INSTANCE_ID -ne "frontend" ]; then
+if [ instance != "frontend" ]; then
     IP=$(aws ec2 describe-instances --instance-ids i-0eca56180912f7295 --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
 else
     IP=$(aws ec2 describe-instances --instance-ids i-0eca56180912f7295 --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
@@ -16,7 +19,7 @@ aws route53 change-resource-record-sets \
     ,"Changes": [{
       "Action"              : "UPSERT"
       ,"ResourceRecordSet"  : {
-        "Name"              : "'mongodb.daws86s-akhil.shop'"
+        "Name"              : "'$instance.daws86s-akhil.shop'"
         ,"Type"             : "A"
         ,"TTL"              : 1
         ,"ResourceRecords"  : [{
