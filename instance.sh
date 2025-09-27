5809736a9946602b9dbded8d3daf3b651822e3b2
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-27 11:52:17 +0530

    message

--- instance.sh
+++ instance.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+AMI="ami-09c813fb71547fc4f"
+SG="sg-0fee42dfd5533e5de"
+
 for instance in $@
 do
-    INSTANCE_ID=$( aws ec2 run-instances --image-id ami-09c813fb71547fc4f --count 1 --instance-type t3.micro --security-group-ids sg-0fee42dfd5533e5de --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text )
+    INSTANCE_ID=$( aws ec2 run-instances --image-id $AMI --count 1 --instance-type t3.micro --security-group-ids $SG --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text )
 done
 
 if [ $instance != "frontend" ]; then
