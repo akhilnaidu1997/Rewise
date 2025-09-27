4202dddbcf8ad85605e28c2b97aae0036eb8c936
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-27 15:05:48 +0530

    message

--- instance.sh
+++ instance.sh
@@ -8,7 +8,7 @@ for instance in $@
 do
     INSTANCE_ID=$( aws ec2 run-instances --image-id $AMI --count 1 --instance-type t3.micro --security-group-ids $SG --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text )
 
-    if [ $instance -ne "frontend" ]; then
+    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD="$instance.$DOMAIN"
     else
