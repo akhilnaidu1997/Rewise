53fe74f69476e74e1ef0dc27f43471221577d1b6
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-28 08:41:04 +0530

    message

--- mongodb.sh
+++ mongodb.sh
@@ -32,11 +32,11 @@ fi
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding repo"
 
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>> $LOG_FILE
 VALIDATE $? "Installing mongodb"
 
 systemctl enable mongod 
-systemctl start mongod 
+systemctl start mongod  &>> $LOG_FILE
 VALIDATE $? "Starting mongodb"
 
 sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
