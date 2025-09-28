2903ace572f57f0a4cfdd93839da3130170393e0
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-28 14:58:31 +0530

    message

---
+++ cart.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USER=$(id -u)
+
+LOG_FOLDER="/var/log/shell-practice"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+MONGOHOST="mongodb.daws86s-akhil.shop"
+SCRIPT_DIR=$PWD 
+SCRIPT_TIME=$(date +%s)
+
+mkdir -p $LOG_FOLDER
+
+echo " Script started executing at : $(date)"\
+
+if [ $USER -ne 0 ]; then
+    echo -e "Please proceed with $R sudo permissions $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 $R failed $N"
+        exit 1
+    else
+        echo -e " $2 is  $Y Successful $N" | tee -a $LOG_FILE
+fi
+}
+
+dnf module disable nodejs -y &>> $LOG_FILE
+VALIDATE $? "Disable nodejs "
+
+dnf module enable nodejs:20 -y &>> $LOG_FILE
+VALIDATE $? "Enable nodej version 20"
+
+dnf install nodejs -y &>> $LOG_FILE
+VALIDATE $? "Nodejs installation"
+
+id=roboshop &>> $LOG_FILE
+if [ $id -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOG_FILE
+    VALIDATE $? "Setting up user"
+else
+    echo "User already exists"
+fi
+
+mkdir /app 
+VALIDATE $? "Creating /app directory"
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip  &>> $LOG_FILE
+VALIDATE $? "downloading to tmp dir"
+
+cd /app 
+unzip /tmp/cart.zip &>> $LOG_FILE
+VALIDATE $? "Unzipping in /app"
+
+npm install &>> $LOG_FILE
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "copy service file"
+
+systemctl daemon-reload
+VALIDATE $? "Daemon reload"
+
+systemctl enable cart  &>> $LOG_FILE
+systemctl start cart
+VALIDATE $? "Enabling and starting cart"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $SCRIPT_TIME))
+
+echo "Script executed in : $TOTAL_TIME"
