12360360286eafd7ef3008ad0e9c100e1c0ab02c
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-28 15:14:11 +0530

    message

--- shipping.sh
+++ shipping.sh
@@ -37,7 +37,7 @@ dnf install maven -y &>> $LOG_FILE
 VALIDATE $? "Installing maven and java"
 
 id=roboshop &>> $LOG_FILE
-if [ $id -ne 0 ]; then
+if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOG_FILE
     VALIDATE $? "Setting up user"
 else
